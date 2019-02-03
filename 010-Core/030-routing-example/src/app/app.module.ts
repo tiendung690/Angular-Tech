64e981db8652c74605224fc15f3d43cd8cdbe100
ben <ben@techoffice.com>
2019-02-03 15:27:14 +0800

    update

---
+++ 010-Core/030-routing-example/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Test1Component,
+    Test2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
